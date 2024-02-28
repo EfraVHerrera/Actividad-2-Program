bd96163a515a46babc338d57710b8f790709010f
EfraVHerrera <159298473+EfraVHerrera@users.noreply.github.com>
2024-02-28 12:35:58 -0400

    Update ACTIVIDAD2.cpp

--- ACTIVIDAD2.cpp
+++ ACTIVIDAD2.cpp
@@ -8,37 +8,35 @@
 using namespace std;
 
 // Estructuras
-struct Libro {
-	string titulo;
-	string autor;
-	string ISBN;
-	int anoPublicacion;
-	string idioma;
-	int edicion;
-	int numPaginas;
+struct SLibro {
+	string sTitulo;
+	string sAutor;
+	string sISBN;
+	int nAnoPublicacion;
+	string sIdioma;
+	int nEdicion;
+	int nNumPaginas;
 };
 
-struct Usuario {
-	int userID;
-	string nombre;
-	string apellido;
-	string email;
-	string fechaRegistro;
+struct SUsuario {
+	int nUserID;
+	string sNombre;
+	string sApellido;
+	string sEmail;
+	string sFechaRegistro;
 };
 
-struct Bibliotecario {
-	int employeeID;
-	string nombre;
-	string apellido;
-	string email;
-	string cargo;
+struct SBibliotecario {
+	int nEmployeeID;
+	string sNombre;
+	string sApellido;
+	string sEmail;
+	string sCargo;
 };
 
 const int NUM_LIBROS = 3;
 const int NUM_USUARIOS = 3;
 const int NUM_BIBLIOTECARIOS = 3;
-
-bool datosCargados = false; // Variable para verificar si los datos ya se han cargado
 
 // Funciones para generar datos aleatorios
 string obtenerElementoAleatorio(string arr[], int size) {
@@ -58,36 +56,35 @@ string generarFechaRegistro(string listaFechasRegistro[], int size) {
 }
 
 // Funciones para llenar las estructuras con datos aleatorios
-void llenarDatos(Libro libros[], string listaTitulos[], string listaNombres[], string listaApellidos[], string listaIdiomas[], bool generarISBN) {
+void llenarDatos(SLibro libros[], string listaTitulos[], string listaNombres[], string listaApellidos[], string listaIdiomas[]) {
 	for (int i = 0; i < NUM_LIBROS; ++i) {
-		libros[i].titulo = obtenerElementoAleatorio(listaTitulos, NUM_LIBROS);
-		libros[i].autor = obtenerElementoAleatorio(listaNombres, 5) + " " + obtenerElementoAleatorio(listaApellidos, 5);
-		if (generarISBN) // Solo generar ISBN si la opción está habilitada
-			libros[i].ISBN = to_string(i + 1) + ",2,3,4,5"; // ISBN falso
-		libros[i].anoPublicacion = obtenerAnoAleatorio();
-		libros[i].idioma = obtenerElementoAleatorio(listaIdiomas, 8); // Se elige un idioma aleatorio
-		libros[i].edicion = rand() % 10 + 1; // Número de edición aleatorio entre 1 y 10
-		libros[i].numPaginas = rand() % 500 + 100; // Número de páginas aleatorio entre 100 y 599
+		libros[i].sTitulo = obtenerElementoAleatorio(listaTitulos, NUM_LIBROS);
+		libros[i].sAutor = obtenerElementoAleatorio(listaNombres, 5) + " " + obtenerElementoAleatorio(listaApellidos, 5);
+		libros[i].sISBN = ""; // Se inicializa vacío
+		libros[i].nAnoPublicacion = obtenerAnoAleatorio();
+		libros[i].sIdioma = obtenerElementoAleatorio(listaIdiomas, 8); // Se elige un idioma aleatorio
+		libros[i].nEdicion = rand() % 10 + 1; // Número de edición aleatorio entre 1 y 10
+		libros[i].nNumPaginas = rand() % 500 + 100; // Número de páginas aleatorio entre 100 y 599
 	}
 }
 
-void llenarDatos(Usuario usuarios[], string listaNombres[], string listaApellidos[], string listaFechasRegistro[], int size) {
+void llenarDatos(SUsuario usuarios[], string listaNombres[], string listaApellidos[], string listaFechasRegistro[], int size) {
 	for (int i = 0; i < NUM_USUARIOS; ++i) {
-		usuarios[i].userID = i + 1;
-		usuarios[i].nombre = obtenerElementoAleatorio(listaNombres, 5);
-		usuarios[i].apellido = obtenerElementoAleatorio(listaApellidos, 5);
-		usuarios[i].email = generarEmail(usuarios[i].nombre, usuarios[i].apellido);
-		usuarios[i].fechaRegistro = generarFechaRegistro(listaFechasRegistro, size); //HOY
+		usuarios[i].nUserID = i + 1;
+		usuarios[i].sNombre = obtenerElementoAleatorio(listaNombres, 5);
+		usuarios[i].sApellido = obtenerElementoAleatorio(listaApellidos, 5);
+		usuarios[i].sEmail = generarEmail(usuarios[i].sNombre, usuarios[i].sApellido);
+		usuarios[i].sFechaRegistro = generarFechaRegistro(listaFechasRegistro, size); //HOY
 	}
 }
 
-void llenarDatos(Bibliotecario bibliotecarios[], string listaNombres[], string listaApellidos[], string listaCargos[], int size) {
+void llenarDatos(SBibliotecario bibliotecarios[], string listaNombres[], string listaApellidos[], string listaCargos[], int size) {
 	for (int i = 0; i < NUM_BIBLIOTECARIOS; ++i) {
-		bibliotecarios[i].employeeID = i + 1;
-		bibliotecarios[i].nombre = obtenerElementoAleatorio(listaNombres, 5);
-		bibliotecarios[i].apellido = obtenerElementoAleatorio(listaApellidos, 5);
-		bibliotecarios[i].email = generarEmail(bibliotecarios[i].nombre, bibliotecarios[i].apellido);
-		bibliotecarios[i].cargo = obtenerElementoAleatorio(listaCargos, size); //HOY
+		bibliotecarios[i].nEmployeeID = i + 1;
+		bibliotecarios[i].sNombre = obtenerElementoAleatorio(listaNombres, 5);
+		bibliotecarios[i].sApellido = obtenerElementoAleatorio(listaApellidos, 5);
+		bibliotecarios[i].sEmail = generarEmail(bibliotecarios[i].sNombre, bibliotecarios[i].sApellido);
+		bibliotecarios[i].sCargo = obtenerElementoAleatorio(listaCargos, size); //HOY
 	}
 }
 
@@ -111,36 +108,32 @@ void imprimirFila(const string& columna1, const string& columna2, const string& 
 }
 
 // Funciones para imprimir los datos de las estructuras en forma de tabla
-void imprimirDatos(Libro *libros, int cant) {
+void imprimirDatos(SLibro *libros, int cant) {
 	cout << "\n--- Listado de libros ---\n";
 	imprimirFila("Título", "Autor", "ISBN", "Año de Publicación", "Idioma");
 	for (int i = 0; i < cant; ++i) {
-		imprimirFila(libros[i].titulo, libros[i].autor, libros[i].ISBN, to_string(libros[i].anoPublicacion), libros[i].idioma);
+		imprimirFila(libros[i].sTitulo, libros[i].sAutor, libros[i].sISBN, to_string(libros[i].nAnoPublicacion), libros[i].sIdioma);
 	}
 }
 
-void imprimirDatos(Usuario *usuarios, int cant) {
+void imprimirDatos(SUsuario *usuarios, int cant) {
 	cout << "\n--- Listado de usuarios ---\n";
 	imprimirFila("ID", "Nombre", "Apellido", "Email", "Fecha de Registro");
 	for (int i = 0; i < cant; ++i) {
-		imprimirFila(to_string(usuarios[i].userID), usuarios[i].nombre, usuarios[i].apellido, usuarios[i].email, usuarios[i].fechaRegistro);
+		imprimirFila(to_string(usuarios[i].nUserID), usuarios[i].sNombre, usuarios[i].sApellido, usuarios[i].sEmail, usuarios[i].sFechaRegistro);
 	}
 }
 
-void imprimirDatos(Bibliotecario *bibliotecarios, int cant) {
+void imprimirDatos(SBibliotecario *bibliotecarios, int cant) {
 	cout << "\n--- Listado de bibliotecarios ---\n";
 	imprimirFila("ID", "Nombre", "Apellido", "Email", "Cargo");
 	for (int i = 0; i < cant; ++i) {
-		imprimirFila(to_string(bibliotecarios[i].employeeID), bibliotecarios[i].nombre, bibliotecarios[i].apellido, bibliotecarios[i].email, bibliotecarios[i].cargo);
+		imprimirFila(to_string(bibliotecarios[i].nEmployeeID), bibliotecarios[i].sNombre, bibliotecarios[i].sApellido, bibliotecarios[i].sEmail, bibliotecarios[i].sCargo);
 	}
 }
 
 // Submenú para el listado
-void submenuListado(Libro libros[], Usuario usuarios[], Bibliotecario bibliotecarios[]) {
-	if (!datosCargados) { // Verificar si los datos ya se han cargado
-		cout << "Primero debe cargar los datos.\n";
-		return;
-	}
+void submenuListado(SLibro libros[], SUsuario usuarios[], SBibliotecario bibliotecarios[]) {
 	char opcion;
 	do {
 		cout << "\n--- Submenú Listado ---\n";
@@ -171,20 +164,20 @@ void submenuListado(Libro libros[], Usuario usuarios[], Bibliotecario biblioteca
 }
 
 // Función para buscar un libro por título
-bool buscarLibro(Libro* libros, int cant, const string& titulo, int index = 0) {
+bool buscarLibro(SLibro* libros, int cant, const string& titulo, int index = 0) {
 	bool encontrado = false;
 	for (int i = 0; i < cant; ++i) {
-		string tituloLibro = libros[i].titulo;
+		string tituloLibro = libros[i].sTitulo;
 		transform(tituloLibro.begin(), tituloLibro.end(), tituloLibro.begin(), ::tolower); // Convertir a minúsculas
 		size_t found = tituloLibro.find(titulo); // Buscar la primera palabra del título
 		if (found != string::npos && (found == 0 || tituloLibro[found - 1] == ' ')) {
-			cout << "Título: " << libros[i].titulo << endl;
-			cout << "Autor: " << libros[i].autor << endl;
-			cout << "ISBN: " << libros[i].ISBN << endl;
-			cout << "Año de Publicación: " << libros[i].anoPublicacion << endl;
-			cout << "Idioma: " << libros[i].idioma << endl;
-			cout << "Edición: " << libros[i].edicion << endl;
-			cout << "Número de Páginas: " << libros[i].numPaginas << endl << endl;
+			cout << "Título: " << libros[i].sTitulo << endl;
+			cout << "Autor: " << libros[i].sAutor << endl;
+			cout << "ISBN: " << libros[i].sISBN << endl;
+			cout << "Año de Publicación: " << libros[i].nAnoPublicacion << endl;
+			cout << "Idioma: " << libros[i].sIdioma << endl;
+			cout << "Edición: " << libros[i].nEdicion << endl;
+			cout << "Número de Páginas: " << libros[i].nNumPaginas << endl << endl;
 			encontrado = true;
 		}
 	}
@@ -193,18 +186,18 @@ bool buscarLibro(Libro* libros, int cant, const string& titulo, int index = 0) {
 
 
 // Función para buscar un usuario por nombre
-bool buscarUsuario(Usuario* usuarios, int cant, const string& nombre, int index = 0) {
+bool buscarUsuario(SUsuario* usuarios, int cant, const string& nombre, int index = 0) {
 	bool encontrado = false;
 	for (int i = 0; i < cant; ++i) {
-		string nombreUsuario = usuarios[i].nombre;
+		string nombreUsuario = usuarios[i].sNombre;
 		transform(nombreUsuario.begin(), nombreUsuario.end(), nombreUsuario.begin(), ::tolower); // Convertir a minúsculas
 		size_t found = nombreUsuario.find(nombre); // Buscar coincidencias en el nombre del usuario
 		if (found != string::npos) {
-			cout << "ID: " << usuarios[i].userID << endl;
-			cout << "Nombre: " << usuarios[i].nombre << endl;
-			cout << "Apellido: " << usuarios[i].apellido << endl;
-			cout << "Email: " << usuarios[i].email << endl;
-			cout << "Fecha de Registro: " << usuarios[i].fechaRegistro << endl << endl;
+			cout << "ID: " << usuarios[i].nUserID << endl;
+			cout << "Nombre: " << usuarios[i].sNombre << endl;
+			cout << "Apellido: " << usuarios[i].sApellido << endl;
+			cout << "Email: " << usuarios[i].sEmail << endl;
+			cout << "Fecha de Registro: " << usuarios[i].sFechaRegistro << endl << endl;
 			encontrado = true;
 		}
 	}
@@ -212,18 +205,18 @@ bool buscarUsuario(Usuario* usuarios, int cant, const string& nombre, int index 
 }
 
 // Función para buscar un bibliotecario por nombre
-bool buscarBibliotecario(Bibliotecario* bibliotecarios, int cant, const string& nombre, int index = 0) {
+bool buscarBibliotecario(SBibliotecario* bibliotecarios, int cant, const string& nombre, int index = 0) {
 	bool encontrado = false;
 	for (int i = 0; i < cant; ++i) {
-		string nombreBibliotecario = bibliotecarios[i].nombre;
+		string nombreBibliotecario = bibliotecarios[i].sNombre;
 		transform(nombreBibliotecario.begin(), nombreBibliotecario.end(), nombreBibliotecario.begin(), ::tolower); // Convertir a minúsculas
 		size_t found = nombreBibliotecario.find(nombre); // Buscar coincidencias en el nombre del bibliotecario
 		if (found != string::npos) {
-			cout << "ID: " << bibliotecarios[i].employeeID << endl;
-			cout << "Nombre: " << bibliotecarios[i].nombre << endl;
-			cout << "Apellido: " << bibliotecarios[i].apellido << endl;
-			cout << "Email: " << bibliotecarios[i].email << endl;
-			cout << "Cargo: " << bibliotecarios[i].cargo << endl << endl;
+			cout << "ID: " << bibliotecarios[i].nEmployeeID << endl;
+			cout << "Nombre: " << bibliotecarios[i].sNombre << endl;
+			cout << "Apellido: " << bibliotecarios[i].sApellido << endl;
+			cout << "Email: " << bibliotecarios[i].sEmail << endl;
+			cout << "Cargo: " << bibliotecarios[i].sCargo << endl << endl;
 			encontrado = true;
 		}
 	}
@@ -231,11 +224,7 @@ bool buscarBibliotecario(Bibliotecario* bibliotecarios, int cant, const string& 
 }
 
 //SUBMENU BUSQUEDA
-void submenuBusqueda(Libro libros[], Usuario usuarios[], Bibliotecario bibliotecarios[]) {
-	if (!datosCargados) { // Verificar si los datos ya se han cargado
-		cout << "Primero debe cargar los datos.\n";
-		return;
-	}
+void submenuBusqueda(SLibro libros[], SUsuario usuarios[], SBibliotecario bibliotecarios[]) {
 	char opcion;
 	do {
 		cout << "\n--- Submenú Búsqueda ---\n";
@@ -303,13 +292,13 @@ int main() {
 	string listaCargos[] = {"Bibliotecario jefe", "Bibliotecario Asistente", "Bibliotecario Junior", "Bibliotecario de tecnología de la información", "Bibliotecario de archivos y preservación"};
 	
 	// Generar estructuras
-	Libro libros[NUM_LIBROS];
-	Usuario usuarios[NUM_USUARIOS];
-	Bibliotecario bibliotecarios[NUM_BIBLIOTECARIOS];
-	
+	SLibro libros[NUM_LIBROS];
+	SUsuario usuarios[NUM_USUARIOS];
+	SBibliotecario bibliotecarios[NUM_BIBLIOTECARIOS];
 	
 	// Menú principal
 	char opcion;
+	bool datosCargados = false; // Bandera para indicar si se han cargado los datos
 	do {
 		cout << "\n--- Menú Principal ---\n";
 		cout << "1. Llenado de datos\n";
@@ -322,21 +311,31 @@ int main() {
 		switch (opcion) {
 		case '1':
 			// Generar datos de libros, usuarios y bibliotecarios
-			llenarDatos(libros, listaLibros, listaNombres, listaApellidos, listaIdiomas, true); // La opción true indica que se generará el ISBN
+			llenarDatos(libros, listaLibros, listaNombres, listaApellidos, listaIdiomas);
 			llenarDatos(usuarios, listaNombres, listaApellidos, listaFechasRegistro, sizeof(listaFechasRegistro) / sizeof(listaFechasRegistro[0]));
 			llenarDatos(bibliotecarios, listaNombres, listaApellidos, listaCargos, sizeof(listaCargos) / sizeof(listaCargos[0]));
-			datosCargados = true; // Establecer que los datos se han cargado
 			cout << "Datos generados exitosamente.\n";
+			datosCargados = true; // Se establece la bandera de datos cargados como verdadera
 			break;
+			
 		case '2':
-			submenuListado(libros, usuarios, bibliotecarios);
+			if (datosCargados) // Solo se puede acceder al listado si los datos están cargados
+				submenuListado(libros, usuarios, bibliotecarios);
+			else
+				cout << "Error: Debe cargar los datos primero (opción 1).\n";
 			break;
+			
 		case '3':
-			submenuBusqueda(libros, usuarios, bibliotecarios);
+			if (datosCargados) // Solo se puede acceder a la búsqueda si los datos están cargados
+				submenuBusqueda(libros, usuarios, bibliotecarios);
+			else
+				cout << "Error: Debe cargar los datos primero (opción 1).\n";
 			break;
+			
 		case '4':
 			cout << "Saliendo del programa...\n";
 			break;
+			
 		default:
 			cout << "Opción no válida. Por favor, seleccione una opción válida.\n";
 		}
